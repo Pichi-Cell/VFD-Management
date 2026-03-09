dc57e6d55afb2ed77f4fda1917a650e2a4afa8b3
pichi-cell <picchilucas7@gmail.com>
2026-03-09 18:13:36 -0300

    dockerization bugfixes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'sonner';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'http://localhost:3000/api',
 });
 
 // Interceptor to add auth token
