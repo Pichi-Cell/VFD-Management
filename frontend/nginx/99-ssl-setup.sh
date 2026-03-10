d566b226ec7a5abf69306006d44d3759ea472d2f
Pichi-Cell <Picchilucas7@gmail.com>
2026-03-10 03:40:34 -0300

    bypassing nginx auto config

--- frontend/nginx/99-ssl-setup.sh
+++ frontend/nginx/99-ssl-setup.sh
@@ -3,8 +3,8 @@ set -e
 
 if [ -f "/etc/nginx/certs/cert.pem" ] && [ -f "/etc/nginx/certs/key.pem" ]; then
     echo "SSL certificates found. Enabling SSL..."
-    cp /etc/nginx/templates/nginx.ssl.conf.template /etc/nginx/conf.d/default.conf
+    cp /etc/nginx/custom-templates/nginx.ssl.conf.template /etc/nginx/conf.d/default.conf
 else
     echo "No SSL certificates found. Falling back to HTTP..."
-    cp /etc/nginx/templates/nginx.http.conf.template /etc/nginx/conf.d/default.conf
+    cp /etc/nginx/custom-templates/nginx.http.conf.template /etc/nginx/conf.d/default.conf
 fi
