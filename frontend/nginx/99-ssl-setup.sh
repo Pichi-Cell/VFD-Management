2c8536cf633f726f57ab868a1922507e0d745627
Pichi-Cell <Picchilucas7@gmail.com>
2026-03-10 04:17:03 -0300

    changed server name to wildcard

--- frontend/nginx/99-ssl-setup.sh
+++ frontend/nginx/99-ssl-setup.sh
@@ -3,8 +3,10 @@ set -e
 
 if [ -f "/etc/nginx/certs/cert.pem" ] && [ -f "/etc/nginx/certs/key.pem" ]; then
     echo "SSL certificates found. Enabling SSL..."
+    rm -f /etc/nginx/conf.d/*.conf
     cp /etc/nginx/custom-templates/nginx.ssl.conf.template /etc/nginx/conf.d/default.conf
 else
     echo "No SSL certificates found. Falling back to HTTP..."
+    rm -f /etc/nginx/conf.d/*.conf
     cp /etc/nginx/custom-templates/nginx.http.conf.template /etc/nginx/conf.d/default.conf
 fi
